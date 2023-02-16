e3c91bde0cd42326cd4c2f76a2c725eb2bc27dd5
p0melo11 <dimabob463@gmail.com>
2023-02-16 23:23:45 +0200

    Добавьте файлы проекта.

---
+++ practic01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Program
+{
+    class Program
+    {
+        delegate int IntGetter();
+        delegate int AverageFromGetters(IntGetter[] valueGetter);
+        delegate int Average(int a, int b, int c);
+
+        public static void Main()
+        {
+            var Add = (int a, int b) => a + b;
+            var Sub = (int a, int b) => a - b;
+            var Mul = (int a, int b) => a * b;
+            var Div = (int a, int b) => a / b;
+
+            Console.WriteLine(Add(1, 5));
+            Console.WriteLine(Sub(6, 5));
+            Console.WriteLine(Mul(2, 5));
+            Console.WriteLine(Div(10, 5));
+
+            IntGetter getter = () => Random.Shared.Next();
+
+            AverageFromGetters averageFromGetters = (items) =>
+            {
+                var sum = 0;
+
+                foreach (var getter in items)
+                {
+                    sum += getter();
+                }
+
+                return sum / items.Length;
+            };
+
+            Average average = (a, b, c) => (a + b + c) / 3;
+
+            IntGetter[] getters = { getter, getter, getter };
+
+            Console.WriteLine(averageFromGetters(getters));
+            Console.WriteLine(average(2, 4, 6));
+        }
+    }
+}
